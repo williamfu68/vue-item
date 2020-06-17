061a1b2464b7e83822efbdeb0f704472c8e60941
williamfu68 <18383173964@163.com>
2020-06-17 18:39:17 +0800

    init my project

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
+<!--App.vue是项目根组件-->
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <!--顶部 Header区域-->
+    <mt-header fixed title="黑马程序员-Vue项目"></mt-header>
+    <!--中间 router-view 区域-->
+    <h3>hello</h3>
+    <!--底部 Tabbar 区域-->
+    <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">消息</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">通讯录</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">设置</span>
+			</a>
+		</nav>
   </div>
 </template>
 
@@ -15,18 +34,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  padding-top:40px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+//按需导入 Mint-UI组件
+import { Header } from 'mint-ui';
+//导入所需样式
+import 'mint-ui/lib/style.css';
+import './assets/lib/dist/css/mui.min.css';
+Vue.component(Header.name, Header);
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
